6fdef2bb7feb43da4a0cf7cf638070ff7713b4bc
Truong Nhat Quang <quang.nhat.truong@skymavis.com>
2025-01-16 13:52:17 +0700

    fix: cofig socket cor origin

--- src/server/socket.ts
+++ src/server/socket.ts
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 export const ioInstance = (httpServer: any) => {
   return new Server(httpServer, {
     cors: {
-      origin:"*",
+    origin:process.env.CLIENT_URL,
       methods: ["GET", "POST"],
     },
   });
